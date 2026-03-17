ff9ff6b6990fa0e9291382f7a784755ee88eb0d0
Aaradhya__13 <aaradhyaved@gmail.com>
2026-03-17 17:42:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,7 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/cart', require('./routes/cart'));
 app.use('/api/orders', require('./routes/orders'));
 
-// ✅ IMPORTANT: Handle ALL routes (including /login)
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
